47abae9d91cf9c86c32dbc5914fbfdcff532f3d7
SORDET Stephane <Stephane.SORDET@cpnv.ch>
2019-04-04 11:42:24 +0200

    Ajout de la concatenation

--- main.c
+++ main.c
@@ -4,17 +4,19 @@
 #define VALEUR_INIT 0
 
 void entreeUtil();
-
+char entree[21] ="";
 int main()
 {
-    char entree[16] ="";
-    int tailleChaine = 0;
+
+    char entree2[21] ="";
+    int tailleChaine,tailleChaine2 =VALEUR_INIT;
     int choixmenu=VALEUR_INIT;
     printf("Bonjour, ce programme est la pour vous familliarisez avec la manipulation de chaines de caracteres.\n\n");
-    printf("Choix 1\n");
-    printf("Choix 1\n");
-    printf("Choix 1\n");
-    printf("Choix 1\n");
+
+    printf("\nChoix 1: Calcule taille chaine\n");
+    printf("Choix 2: Concatenation de 2 chaines\n");
+    printf("Choix 3\n");
+    printf("Choix 4\n");
     printf("Choisissez une option svp: ");
     scanf("%i",&choixmenu);
     system("cls");
@@ -27,6 +29,16 @@ int main()
 
         break;
     case 2:
+        entreeUtil();
+
+        do {
+        printf("Entrez la chaie a concatener (max. 20): ");
+        scanf("%s",&entree2);
+        tailleChaine2 = strlen(entree2);
+    } while (tailleChaine2>20);
+
+        strcat(entree, entree2);
+        printf("%s",entree);
 
         break;
     case 3:
@@ -43,12 +55,11 @@ int main()
 }
 void entreeUtil()
 {
-    char entree[16] ="";
+
     int tailleChaine = 0;
     do {
-        printf("Entrez une chaine de caracteres (max. 15): ");
+        printf("Entrez une chaine de caracteres (max. 20): ");
         scanf("%s",&entree);
-        printf("%s\n",entree);
         tailleChaine = strlen(entree);
     } while (tailleChaine>15);
 }
