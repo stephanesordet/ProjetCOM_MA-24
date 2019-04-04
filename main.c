b2f08bfa457e71768934a2ffec14a9f70d7d09f9
SORDET Stephane <Stephane.SORDET@cpnv.ch>
2019-04-04 10:41:55 +0200

    Ajout switch + strlen

--- main.c
+++ main.c
@@ -1,8 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define VALEUR_INIT 0
+
+void entreeUtil();
 
 int main()
 {
+    char entree[16] ="";
+    int tailleChaine = 0;
+    int choixmenu=VALEUR_INIT;
+    printf("Bonjour, ce programme est la pour vous familliarisez avec la manipulation de chaines de caracteres.\n\n");
+    printf("Choix 1\n");
+    printf("Choix 1\n");
+    printf("Choix 1\n");
+    printf("Choix 1\n");
+    printf("Choisissez une option svp: ");
+    scanf("%i",&choixmenu);
+    system("cls");
 
+    switch (choixmenu)
+    {
+    case 1:
+        entreeUtil();
+        printf("Votre entree contient %i caractères",tailleChaine);
+
+        break;
+    case 2:
+
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        break;
+    default:
+
+        break;
+    }
     return 0;
+}
+void entreeUtil()
+{
+    char entree[16] ="";
+    int tailleChaine = 0;
+    do {
+        printf("Entrez une chaine de caracteres (max. 15): ");
+        scanf("%s",&entree);
+        printf("%s\n",entree);
+        tailleChaine = strlen(entree);
+    } while (tailleChaine>15);
 }
